c93e8b0d0a99012aa8245ebf0da1565d8ea33c10
mmf1982 <5464@gmx.net>
2020-09-18 10:50:31 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("requirements_dev.txt", "r") as fid:
+    requirements = fid.read().split("\n")
+
+setup(
+    name="QTnetCDF",
+    version="0.0.1",
+    author="Martina M Friedrich",
+    author_email="5464@gmx.net",
+    description="A package to interactively view netCDF and hdf4/ hdf5 files using QT",
+    #long_description=readme,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/mmf1982/QTnetCDF/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
